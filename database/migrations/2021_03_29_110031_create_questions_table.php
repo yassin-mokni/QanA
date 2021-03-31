c1f09afb69a2e17f8765a7cd2bad7c31f6da047e
Yassine Mokni <thexlearner.webdev@gmail.com>
2021-03-31 13:07:18 +0100

    add is _correct column to question table

--- database/migrations/2021_03_29_110031_create_questions_table.php
+++ database/migrations/2021_03_29_110031_create_questions_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateQuestionsTable extends Migration
 {
@@ -17,6 +17,7 @@ class CreateQuestionsTable extends Migration
             $table->increments('id');
             $table->text('content');
             $table->boolean('answered')->default(0);
+            $table->boolean('is_correct')->default(0);
             $table->timestamps();
         });
     }
